54a5987a12d8f9044997c26822e4b774853b5b3c
Sahil Dhiman <sahildhiman76866@gmail.com>
2024-09-14 20:21:04 +0530

    first project

--- Front-end/src/components/Footer.jsx
+++ Front-end/src/components/Footer.jsx
@@ -2,53 +2,43 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 
 import styles from '../styles/styles.module.css';
+import { ImFacebook2 } from "react-icons/im";
+import { RiInstagramFill } from "react-icons/ri";
+import { BsLinkedin } from "react-icons/bs";
+import { ImGithub } from "react-icons/im";
+import { MdEmail } from "react-icons/md";
 
 const Footer = () => {
   return (
     <>
-      {/* 
+
       <section className="container" id="foterbox">
 
-        {/* <div className="mainfoter row py-3 "> */}
-      {/*  <div className={styles.mainfoter}>
+        <div className="mainfoter row py-3 ">
 
-       
+          <footer className={styles.footer}>
+            <hr className="container  " />
 
-          <div className={`${styles.foterbox} col-md-2 text-center`}>
-            <h3><span>Quick links</span></h3>
-            <Link to="#"> <i className="fas fa-arrow-right"></i> Home </Link>
-      
-            <Link to="#"> <i className="fas fa-arrow-right"></i> Contact </Link>
-          </div>
+            <div className={`${styles.credit} bg-slate-800 p-4`}>
+              <div className='flex p-2 text-3xl gap-5 justify-center  text-white my-3 rounded-full '>
+                <Link to={'https://www.facebook.com/profile.php?id=100027765728262&mibextid=ZbWKwL'} target='_blank'> <ImFacebook2 /></Link>
+                <Link to={'https://www.instagram.com/name_is_sahil_7?igsh=ejh1aW4wbnIwcm0='} target='_blank'>  <RiInstagramFill /> </Link>
+                <Link to={'mailto:sahildhiman76866@gmail.com'} target='_blank'> <MdEmail/> </Link>
+                <Link to={'https://github.com/sahil76866'} target='_blank'> <ImGithub /> </Link>
+                <Link to={'https://www.linkedin.com/in/sahil-dhiman-b249a5244/'} target='_blank'> <BsLinkedin /> </Link>
+              </div>
 
-          <div className={`${styles.foterbox}col-md-4  text-center`}>
-            <h3> <span>Contact info</span></h3>
-            <Link to="#"> <i className="fas fa-phone"></i> +91 79739 84439</Link>
-            <Link to=""> <i className="fas fa-envelope"></i> sahildhiman76866@gmail.com </Link>
-                <Link to="#"> <i className="fas fa-map-marker-alt"></i> Chandigarh, india </Link>
-          </div>
+              <p className='text-white'> Developed By
+                <span className=" italic text-red-400 " >  Mr. Sahil Dhiman  </span>
+                | all &copy; copy rights reserved
 
-          <div className={`${styles.foterbox}col-md-3  text-center`}>
-            <h3><span>Follow us</span></h3>
-            <Link to="https://www.facebook.com/profile.php?id=100027765728262&mibextid=ZbWKwL">
-              <i className="fab fa-facebook-f"></i> facebook </Link>
-            <Link to="https://www.instagram.com/name_is_sahil_7?igsh=ejh1aW4wbnIwcm0=">
-              <i className="fab fa-instagram"></i> instagram </Link>
-            <Link to="https://www.whatsapp.com/"> <i className="fab fa-whatsapp">
-            </i> whatsapp </Link>
-          </div>
-        </div> */}
-      {/* <footer className={styles.footer}> */}
-      <hr className="container  " />
+              </p>
+            </div>
 
-      <div className={`${styles.credit} bg-slate-300 p-4`}> Created by
-        <span className={styles.span}> Mr. Web Developer  </span>
-        | all &copy; copy rights reserved </div>
-
-      <hr className="container" />
-      {/* </section> */}
-
-      {/* </footer> */}
+            <hr className="container" />
+          </footer>
+        </div>
+      </section >
 
 
     </>
